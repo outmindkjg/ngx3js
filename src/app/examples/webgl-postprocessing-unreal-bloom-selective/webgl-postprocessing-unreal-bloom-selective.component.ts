b0f52746155e593eb366e3f685527e2edc600570
RainMan <outmind0@gmail.com>
2021-05-23 18:58:24 +0900

    add 20210523

--- src/app/examples/webgl-postprocessing-unreal-bloom-selective/webgl-postprocessing-unreal-bloom-selective.component.ts
+++ src/app/examples/webgl-postprocessing-unreal-bloom-selective/webgl-postprocessing-unreal-bloom-selective.component.ts
@@ -6,10 +6,38 @@ import { BaseComponent } from '../../three';
   templateUrl: './webgl-postprocessing-unreal-bloom-selective.component.html',
   styleUrls: ['./webgl-postprocessing-unreal-bloom-selective.component.scss']
 })
-export class WebglPostprocessingUnrealBloomSelectiveComponent extends BaseComponent<{}> {
+export class WebglPostprocessingUnrealBloomSelectiveComponent extends BaseComponent<{
+  exposure: number,
+  bloomStrength: number,
+  bloomThreshold: number,
+  bloomRadius: number,
+  scene: string
+}> {
 
   constructor() {
-    super({},[]);
+    super({
+      exposure: 1,
+      bloomStrength: 5,
+      bloomThreshold: 0,
+      bloomRadius: 0,
+      scene: "Scene with Glow"
+    },[]);
   }
 
+  vertexshader = `
+  varying vec2 vUv;
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  }
+  `;
+
+  fragmentshader = `
+  uniform sampler2D baseTexture;
+  uniform sampler2D bloomTexture;
+  varying vec2 vUv;
+  void main() {
+    gl_FragColor = ( texture2D( baseTexture, vUv ) + vec4( 1.0 ) * texture2D( bloomTexture, vUv ) );
+  }
+  `;
 }
