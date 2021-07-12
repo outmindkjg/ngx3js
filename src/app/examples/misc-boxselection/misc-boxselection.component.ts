5f45a291ec3e04faf68f3b4b03844910eb21e21d
RainMan <outmind0@gmail.com>
2021-07-12 22:45:06 +0900

    add 20210712

--- src/app/examples/misc-boxselection/misc-boxselection.component.ts
+++ src/app/examples/misc-boxselection/misc-boxselection.component.ts
@@ -12,4 +12,34 @@ export class MiscBoxselectionComponent extends BaseComponent<{}> {
     super({},[]);
   }
 
+  ngOnInit() {
+    this.boxInfos = [];
+    for ( let i = 0; i < 200; i ++ ) {
+      this.boxInfos.push({
+        position : {
+          x : Math.random() * 1600 - 800,
+          y : Math.random() * 900 - 450,
+          z : Math.random() * 900 - 500
+        },
+        rotation : {
+          x : Math.random() * 2 * 180,
+          y : Math.random() * 2 * 180,
+          z : Math.random() * 2 * 180,
+        },
+        scale : {
+          x : Math.random() * 2 + 1,
+          y : Math.random() * 2 + 1,
+          z : Math.random() * 2 + 1,
+        },
+        color : Math.random() * 0xffffff
+      });
+    }
+  }
+  boxInfos : {
+    position : { x : number, y : number, z : number};
+    rotation : { x : number, y : number, z : number};
+    scale : { x : number, y : number, z : number};
+    color : number;
+  }[] = [];
+
 }
