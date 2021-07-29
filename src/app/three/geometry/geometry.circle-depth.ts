bc9c3046f306064327e19b53e3dd1799199580b3
RainMan <outmind0@gmail.com>
2021-07-29 22:52:20 +0900

    add 20210729

--- src/app/three/geometry/geometry.circle-depth.ts
+++ src/app/three/geometry/geometry.circle-depth.ts
@@ -4,52 +4,50 @@ import * as THREE from 'three';
  * CircleDepth geometry
  */
 export class CircleDepthGeometry extends THREE.CircleGeometry {
-
-
-  /**
-   * @param [width=1]
-   * @param [height=1]
-   * @param [depth=1]
-   * @param [widthSegments=1]
-   * @param [heightSegments=1]
-   */
-  constructor(radius?: number, depth? : number , segments?: number, thetaStart?: number, thetaLength?: number) {
-    super(radius, segments, thetaStart, thetaLength);
-    this.type = 'CircleDepthGeometry';
-    depth = Math.max(0.001, depth);
-    const depthGeometry = this.clone();
-    depthGeometry.translate(0,0, - depth);
-    const newIndices : number[] = [];
-    const positionCount = this.getAttribute('position').count ;
-    const oldIndices = depthGeometry.getIndex().array;
-    for(let i = oldIndices.length -1 ; i >= 0 ; i--) {
-      newIndices.push(oldIndices[i] + positionCount);
-    }
-    for(let i = 0 ; i < positionCount ; i++) {
-      const topEnd = (i+1) >= positionCount ? i - positionCount + 1 : i + 1;
-      newIndices.push(i,i + positionCount, topEnd);
-      newIndices.push(topEnd, i + positionCount, positionCount + ((i + 1) % positionCount));
-    }
-    depthGeometry.setIndex(newIndices);
-    const indices = this.mergeBufferAttribute(this.getIndex(), depthGeometry.getIndex());
-    const vertices = this.mergeBufferAttribute(this.getAttribute('position'), depthGeometry.getAttribute('position'));
-    const normals = this.mergeBufferAttribute(this.getAttribute('normal'), depthGeometry.getAttribute('normal'));
-    const uvs = this.mergeBufferAttribute(this.getAttribute('uv'), depthGeometry.getAttribute('uv'));
-    this.setIndex( indices );
-    this.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-    this.setAttribute( 'normal', new THREE.Float32BufferAttribute( normals, 3 ) );
-    this.setAttribute( 'uv', new THREE.Float32BufferAttribute( uvs, 2 ) );
-  }
-
-	protected mergeBufferAttribute(from : THREE.BufferAttribute | THREE.InterleavedBufferAttribute, to : THREE.BufferAttribute | THREE.InterleavedBufferAttribute): number[] {
-		const result : number[] = [];
-		for(let i = 0; i < from.array.length ; i++) {
-			result.push(from.array[i]);
+	/**
+	 * @param [width=1]
+	 * @param [height=1]
+	 * @param [depth=1]
+	 * @param [widthSegments=1]
+	 * @param [heightSegments=1]
+	 */
+	constructor(radius?: number, depth?: number, segments?: number, thetaStart?: number, thetaLength?: number) {
+		super(radius, segments, thetaStart, thetaLength);
+		this.type = 'CircleDepthGeometry';
+		depth = Math.max(0.001, depth);
+		const indices = this.duplArray(this.getIndex().array);
+		const vertices = this.duplArray(this.getAttribute('position').array);
+		const normals = this.duplArray(this.getAttribute('normal').array);
+		const uvs = this.duplArray(this.getAttribute('uv').array);
+		const tmpIndices = this.getIndex().array;
+		const positionCount = this.getAttribute('position').count;
+		let startidx = tmpIndices.length;
+		for (let i = 0; i < tmpIndices.length; i++) {
+			indices[startidx + i] = positionCount + tmpIndices[startidx - i - 1];
 		}
-		for(let i = 0; i < to.array.length ; i++) {
-			result.push(to.array[i]);
+    console.log(positionCount);
+		for (let i = 0; i < positionCount; i++) {
+			indices.push(i, positionCount + i, i + 1);
+			indices.push(i + 1, positionCount + i , positionCount + ((i + 1 >= positionCount) ? 1 : i + 1 ));
 		}
-		return result ;
+    startidx = positionCount * 3;
+		for (let i = 0; i < startidx; i += 3) {
+			vertices[startidx + i + 2] -= depth;
+		}
+		this.setIndex(indices);
+		this.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+		this.setAttribute('normal', new THREE.Float32BufferAttribute(normals, 3));
+		this.setAttribute('uv', new THREE.Float32BufferAttribute(uvs, 2));
 	}
 
+	protected duplArray(data: ArrayLike<number>): number[] {
+		const result: number[] = [];
+		for (let i = 0; i < data.length; i++) {
+			result.push(data[i]);
+		}
+		for (let i = 0; i < data.length; i++) {
+			result.push(data[i]);
+		}
+		return result;
+	}
 }
