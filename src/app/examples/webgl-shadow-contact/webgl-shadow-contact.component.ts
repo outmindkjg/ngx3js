6f829e49e869774d0cc57577d843c191822ae322
RainMan <outmind0@gmail.com>
2021-08-07 15:20:18 +0900

    add 20210807

--- src/app/examples/webgl-shadow-contact/webgl-shadow-contact.component.ts
+++ src/app/examples/webgl-shadow-contact/webgl-shadow-contact.component.ts
@@ -6,10 +6,44 @@ import { BaseComponent } from '../../three';
   templateUrl: './webgl-shadow-contact.component.html',
   styleUrls: ['./webgl-shadow-contact.component.scss']
 })
-export class WebglShadowContactComponent extends BaseComponent<{}> {
+export class WebglShadowContactComponent extends BaseComponent<{
+  shadow: {
+    blur: number,
+    darkness: number,
+    opacity: number,
+  },
+  plane: {
+    color: string,
+    opacity: number,
+  },
+  showWireframe: boolean,
+}> {
 
   constructor() {
-    super({},[]);
+    super({
+      shadow: {
+        blur: 3.5,
+        darkness: 1,
+        opacity: 1,
+      },
+      plane: {
+        color: '#ffffff',
+        opacity: 1,
+      },
+      showWireframe: false,
+    },[
+      { name : 'shadow', type : 'folder', control : 'shadow', children : [
+        { name : 'blur', type : 'number', min : 0, max : 15, step : 0.1},
+        { name : 'darkness', type : 'number', min : 1, max : 5, step : 0.1},
+        { name : 'opacity', type : 'number', min : 0, max : 1, step : 0.01}
+      ], isOpen : true },
+
+      { name : 'plane', type : 'folder', control : 'plane', children : [
+        { name : 'color', type : 'color'},
+        { name : 'opacity', type : 'number', min : 0, max : 1, step : 0.01}
+      ], isOpen : true },
+      { name : 'showWireframe', type : 'checkbox'}
+    ]);
   }
 
 }
--- src/app/examples/webgl-test-memory/webgl-test-memory.component.ts
+++ src/app/examples/webgl-test-memory/webgl-test-memory.component.ts
@@ -3,38 +3,39 @@ import { CanvasTexture, Material, Mesh, MeshBasicMaterial, SphereGeometry } from
 import { BaseComponent, RendererTimer } from '../../three';
 
 @Component({
-  selector: 'app-webgl-test-memory',
-  templateUrl: './webgl-test-memory.component.html',
-  styleUrls: ['./webgl-test-memory.component.scss']
+	selector: 'app-webgl-test-memory',
+	templateUrl: './webgl-test-memory.component.html',
+	styleUrls: ['./webgl-test-memory.component.scss'],
 })
 export class WebglTestMemoryComponent extends BaseComponent<{}> {
+	constructor() {
+		super({}, []);
+	}
 
-  constructor() {
-    super({},[]);
-  }
-
-  createImage() {
-    const canvas = document.createElement( 'canvas' );
-    canvas.width = 256;
-    canvas.height = 256;
-    const context = canvas.getContext( '2d' );
-    context.fillStyle = 'rgb(' + Math.floor( Math.random() * 256 ) + ',' + Math.floor( Math.random() * 256 ) + ',' + Math.floor( Math.random() * 256 ) + ')';
-    context.fillRect( 0, 0, 256, 256 );
-    return canvas;
-  }
-  lastMesh : Mesh = null;
-  onRender(timer : RendererTimer) {
-    super.onRender(timer);
-    if (this.lastMesh !== null) {
-      this.lastMesh.parent.remove(this.lastMesh);
-      this.lastMesh.geometry.dispose();
-      (this.lastMesh.material as Material).dispose();
-    }
-    const geometry = new SphereGeometry( 50, Math.random() * 64, Math.random() * 32 );
-    const texture = new CanvasTexture( this.createImage() );
-    const material = new MeshBasicMaterial( { map: texture, wireframe: true } );
-    const mesh = new Mesh( geometry, material );
-    this.mesh.getObject3d().add(mesh);
-    this.lastMesh = mesh;
-  }
+	createImage() {
+		const canvas = document.createElement('canvas');
+		canvas.width = 256;
+		canvas.height = 256;
+		const context = canvas.getContext('2d');
+		context.fillStyle = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+		context.fillRect(0, 0, 256, 256);
+		return canvas;
+	}
+	lastMesh: Mesh = null;
+	onRender(timer: RendererTimer) {
+		super.onRender(timer);
+		if (this.mesh !== null) {
+			if (this.lastMesh !== null) {
+				this.lastMesh.parent.remove(this.lastMesh);
+				this.lastMesh.geometry.dispose();
+				(this.lastMesh.material as Material).dispose();
+			}
+			const geometry = new SphereGeometry(50, Math.random() * 64, Math.random() * 32);
+			const texture = new CanvasTexture(this.createImage());
+			const material = new MeshBasicMaterial({ map: texture, wireframe: true });
+			const mesh = new Mesh(geometry, material);
+			this.mesh.getObject3d().add(mesh);
+			this.lastMesh = mesh;
+		}
+	}
 }
