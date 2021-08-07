61a5cf8e627fb5d9ea8728afab681b4be0f02603
RainMan <outmind0@gmail.com>
2021-08-07 11:17:04 +0900

    add 20210807

--- src/app/examples/webgl-refraction/webgl-refraction.component.ts
+++ src/app/examples/webgl-refraction/webgl-refraction.component.ts
@@ -24,7 +24,7 @@ export class WebglRefractionComponent extends BaseComponent<{}> {
     // this.refractorMaterial.side = DoubleSide;
   }
 
-  refractorMaterial : ShaderMaterial;
+  refractorMaterial : ShaderMaterial = null;
 
   onRender(timer : RendererTimer) {
     super.onRender(timer);
