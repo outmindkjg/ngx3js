d1aac97d89b1a19b0916c7c6fbc0711414001bcb
RainMan <outmind0@gmail.com>
2021-09-28 21:49:19 +0900

    add 20210926

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,44 +4,50 @@ import { Subscription } from 'rxjs';
 import { ThreeUtil } from 'ngx3js';
 
 export interface SearchMenu {
-  id: string;
-  safeId?: string;
-  name: string;
-  image: string;
-  selected: boolean;
-  tags: string;
+	id: string;
+	safeId?: string;
+	name: string;
+	image: string;
+	selected: boolean;
+	tags: string;
 }
 
 export interface SearchMenuTop {
-  name: string;
-  children: SearchMenu[];
+	name: string;
+	children: SearchMenu[];
 }
 
+/**
+ * Component
+ */
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, AfterViewInit {
+	private subscription: Subscription;
 
-  private subscription: Subscription;
+	constructor(private router: Router, private ele: ElementRef) {
+		this.subscription = this.router.events.subscribe((event: any) => {});
+	}
 
-  constructor(private router: Router, private ele: ElementRef) {
-    this.subscription = this.router.events.subscribe((event: any) => {
-    });
-  }
+	ngOnDestroy(): void {
+		if (this.subscription !== null) {
+			this.subscription.unsubscribe();
+			this.subscription = null;
+		}
+	}
 
-  ngOnDestroy(): void {
-    if (this.subscription !== null) {
-      this.subscription.unsubscribe();
-      this.subscription = null;
-    }
-  }
+	/**
+	 * on init
+	 */
+	ngOnInit(): void {
+		ThreeUtil.setAssetUrl('assets/examples/');
+	}
 
-  ngOnInit(): void {
-    ThreeUtil.setAssetUrl('assets/examples/');
-  }
-
-  ngAfterViewInit() {}
-
+	/**
+	 * after view init
+	 */
+	ngAfterViewInit() {}
 }
--- src/app/examples/css2d-label/css2d-label.component.ts
+++ src/app/examples/css2d-label/css2d-label.component.ts
@@ -2,28 +2,42 @@ import { Component } from '@angular/core';
 import { BaseComponent, MeshComponent, RendererTimer, THREE } from 'ngx3js';
 
 @Component({
-  selector: 'app-css2d-label',
-  templateUrl: './css2d-label.component.html',
-  styleUrls: ['./css2d-label.component.scss']
+	selector: 'app-css2d-label',
+	templateUrl: './css2d-label.component.html',
+	styleUrls: ['./css2d-label.component.scss'],
 })
 export class Css2dLabelComponent extends BaseComponent<{}> {
+	/**
+	 * Creates an instance of css2d label component.
+	 *
+	 */
+	constructor() {
+		super({}, []);
+	}
 
-  constructor() {
-    super({},[]);
-  }
+	/**
+	 * Sets moon
+	 *
+	 * @param mesh
+	 */
+	setMoon(mesh: MeshComponent) {
+		this.moon = mesh.getObject3d();
+	}
 
-  setMoon(mesh : MeshComponent) {
-    this.moon = mesh.getObject3d();
-  }
+	/**
+	 * Moon  of css2d label component
+	 */
+	moon: THREE.Object3D = null;
 
-  moon : THREE.Object3D = null;
-
-  onRender(timer : RendererTimer) {
-    super.onRender(timer);
-    if (this.moon !== null) {
-      const elapsed = timer.elapsedTime;
-      this.moon.position.set( Math.sin( elapsed ) * 5, 0, Math.cos( elapsed ) * 5 );
-    }
-  }
-
+	/**
+	 * Determines whether render on
+	 * @param timer
+	 */
+	onRender(timer: RendererTimer) {
+		super.onRender(timer);
+		if (this.moon !== null) {
+			const elapsed = timer.elapsedTime;
+			this.moon.position.set(Math.sin(elapsed) * 5, 0, Math.cos(elapsed) * 5);
+		}
+	}
 }
