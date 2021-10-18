b846bb58b3073c90874c89ec9d8eac4ea06eb6ee
MacXL <outmind0@gmail.com>
2021-10-18 18:03:31 +0900

    threejs version 133.1

---
+++ src/app/examples/webgl-loader-gltf-sheen/webgl-loader-gltf-sheen.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { BaseComponent, MeshComponent } from 'ngx3js';
+
+@Component({
+	selector: 'app-webgl-loader-gltf-sheen',
+	templateUrl: './webgl-loader-gltf-sheen.component.html',
+	styleUrls: ['./webgl-loader-gltf-sheen.component.scss'],
+})
+export class WebglLoaderGltfSheenComponent extends BaseComponent<{ sheen : number}> {
+	constructor() {
+		super({ sheen : 1 }, [
+			{ name : 'sheen', type : 'number', min : 0, max : 1 ,  finishChange : () => {
+				if (this.material !== null) {
+					this.material.sheen = this.controls.sheen;
+				}
+			}}
+		]);
+	}
+
+	material : any;
+
+	setMesh(mesh : MeshComponent) {
+		super.setMesh(mesh);
+		const object = this.meshObject3d.getObjectByName( 'SheenChair_fabric' ) as any;
+		if (object) {
+			this.material = object.material;
+		}
+	}
+
+
+}
