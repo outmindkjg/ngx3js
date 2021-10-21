c55c43916d1e0f77507bfa183d1a627bd6bbb94b
MacXL <outmind0@gmail.com>
2021-10-21 17:45:47 +0900

    add 20211021

--- src/app/examples/webgl-loader-texture-lottie/webgl-loader-texture-lottie.component.ts
+++ src/app/examples/webgl-loader-texture-lottie/webgl-loader-texture-lottie.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
-import { BaseComponent } from 'ngx3js';
+import { BaseComponent, TextureComponent } from 'ngx3js';
 
 @Component({
 	selector: 'app-webgl-loader-texture-lottie',
 	templateUrl: './webgl-loader-texture-lottie.component.html',
 	styleUrls: ['./webgl-loader-texture-lottie.component.scss'],
 })
-export class WebglLoaderTextureLottieComponent extends BaseComponent<{}> {
+export class WebglLoaderTextureLottieComponent extends BaseComponent<{
+	pause : () => void,
+	play : () => void,
+	scrubber : number;
+}> {
 	constructor() {
-		super({}, []);
+		super({
+			pause : () => {
+				if (this.animation !== null) {
+					this.animation.pause();
+				}
+			},
+			play : () => {
+				if (this.animation !== null) {
+					this.animation.play();
+				}
+			},
+			scrubber : 0
+		}, [
+			{ name : 'pause', type : 'button'},
+			{ name : 'play', type : 'button'},
+			{ name : 'scrubber', type : 'number', min : 0, max : 100, listen : true, change : () => {
+				if (this.animation !== null) {
+					this.animation.goToAndStop( this.controls.scrubber / 100 * this.totalFrames , true );
+				}
+			}},
+		]);
 	}
+
+	setLottie( texture : TextureComponent) {
+		this.animation = (texture.getTexture() as any).animation;
+		this.totalFrames = this.animation?.totalFrames;
+		this.animation?.addEventListener( 'enterFrame', () => {
+			this.controls.scrubber = this.animation.currentFrame / this.totalFrames * 100;
+		} );
+
+	}
+
+	animation : any = null;
+	totalFrames : number = 1;
+
 }
